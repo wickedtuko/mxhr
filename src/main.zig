d5ed4cc0ac924ee19b4b4790732d63833c3fd912
Tuko <tuko.ustx@gmail.com>
2026-01-27 20:46:43 -0600

    Add to notification area

--- src/main.zig
+++ src/main.zig
@@ -3,6 +3,8 @@ const win32 = @import("win32").everything;
 const L = win32.L;
 const HWND = win32.HWND;
 
+const WM_TRAYICON = win32.WM_USER + 1;
+
 pub export fn wWinMain(
     hInstance: win32.HINSTANCE,
     _: ?win32.HINSTANCE,
@@ -44,6 +46,22 @@ pub export fn wWinMain(
         null, // Additional application data
     ) orelse win32.panicWin32("CreateWindow", win32.GetLastError());
 
+    // Add system tray icon
+    var nid: win32.NOTIFYICONDATAW = undefined;
+    nid.cbSize = @sizeOf(win32.NOTIFYICONDATAW);
+    nid.hWnd = hwnd;
+    nid.uID = 1;
+    nid.uFlags = .{ .MESSAGE = 1, .ICON = 1, .TIP = 1 };
+    nid.uCallbackMessage = WM_TRAYICON;
+    nid.hIcon = win32.LoadIconW(null, win32.IDI_APPLICATION);
+    const tooltip = L("mxhr Application");
+    @memcpy(nid.szTip[0..tooltip.len], tooltip);
+    nid.szTip[tooltip.len] = 0;
+
+    if (win32.Shell_NotifyIconW(.ADD, &nid) == 0) {
+        win32.panicWin32("Shell_NotifyIcon", win32.GetLastError());
+    }
+
     _ = win32.ShowWindow(hwnd, .{ .SHOWNORMAL = 1 });
 
     var msg: win32.MSG = undefined;
@@ -61,7 +79,29 @@ fn WindowProc(
     lParam: win32.LPARAM,
 ) callconv(.winapi) win32.LRESULT {
     switch (uMsg) {
+        WM_TRAYICON => {
+            if (lParam == win32.WM_LBUTTONDOWN) {
+                // Left click on tray icon - show/hide window
+                if (win32.IsWindowVisible(hwnd) != 0) {
+                    _ = win32.ShowWindow(hwnd, win32.SW_HIDE);
+                } else {
+                    _ = win32.ShowWindow(hwnd, win32.SW_SHOW);
+                    _ = win32.SetForegroundWindow(hwnd);
+                }
+            } else if (lParam == win32.WM_RBUTTONDOWN) {
+                // Right click on tray icon - could show context menu
+                _ = win32.SetForegroundWindow(hwnd);
+            }
+            return 0;
+        },
         win32.WM_DESTROY => {
+            // Remove system tray icon
+            var nid: win32.NOTIFYICONDATAW = undefined;
+            nid.cbSize = @sizeOf(win32.NOTIFYICONDATAW);
+            nid.hWnd = hwnd;
+            nid.uID = 1;
+            _ = win32.Shell_NotifyIconW(.DELETE, &nid);
+
             win32.PostQuitMessage(0);
             return 0;
         },
